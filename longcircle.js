f39752deb120a48ca8b93be56ac3bbd1dfdaea82
dlluncor <david.lluncor@gmail.com>
2013-04-20 13:10:07 -0700

    new commit.

--- longcircle.js
+++ longcircle.js
@@ -1,4 +1,8 @@
 		var ws;
+		var numPerSec = 7;
+		var timeTilGas = 120 * numPerSec; // Number of indices until we trigger gas.
+		var speedRange = 3 * numPerSec; // Number of indices for speed window.
+		var enoughIndices = speedRange * 2 + 10;
 
 		Ctrl = function() {
 		  this.ind = 0;
@@ -11,8 +15,16 @@
 		  this.slowDownCalled = false;
 		  this.findParkingCalled = 0; // index at which find parking was called.
 		  this.messages = [];
-		
 		  this.coastingRangeTime = 0; // number of times I am coasting.
+
+		  // Gasoline.
+		  this.startAskGas = false;
+		  this.startAskInd = 0;
+		  this.promptedGas = false; // Do it only once.
+		
+		  this.akivasWay = false;
+		  this.downSample = 3;
+		  this.alwaysInd = 0;
 		};
 
 		// Trigger find parking when you are going below 18 mph on average,
@@ -24,45 +36,57 @@
 			  return;
 			}
 
+			this.alwaysInd += 1;
+
+			if ((this.alwaysInd % this.downSample) != 0) {
+			  return;
+			}
+
+			// Now we can sample.
 			this.messages.push(response);
 			this.ind += 1;
 
-			if (this.ind < 41) {return;}
+			this.askGas();
+			if (this.ind < enoughIndices) {return;}
 			// I have enough time to start computing.
-			if (this.ind == 42) {
+			if (this.ind == enoughIndices + 1) {
 			  // set up averages.
-			  for (var i = this.startNow; i < this.startNow + 20; i++) { 
+			  for (var i = this.startNow; i < this.startNow + speedRange; i++) { 
 				this.sumNowSpeed += this.getSpeed(i) ; 
 			  }
-			  for (var i = this.startSlow; i < this.startSlow + 20; i++) { 
+			  for (var i = this.startSlow; i < this.startSlow + speedRange; i++) { 
 				this.sumSlowSpeed += this.getSpeed(i) ; 
 			  }
 			}
-
 			var rightNowSpeed = this.getSpeed(this.ind - 1);
 			var prevSpeed = this.getSpeed(this.ind - 2);
 			if (rightNowSpeed > 9) {
-			// Using Akiva heuristic 1.
-			if (rightNowSpeed > 13 && rightNowSpeed < 18) {
-			  this.coastingRangeTime += 1;	
-			} else {
-			  this.coastingRangeTime = 0;
-			}
-			// If we have been coasting for lets say 10 seconds = 70 beats.
-			if (this.coastingRangeTime > 69) {
-			  this.findParkingMaybe();
-			}
 
+			  if (this.akivasWay == true) {
+
+			    // Using Akiva heuristic 1.
+			    if (rightNowSpeed > 13 && rightNowSpeed < 18) {
+			      this.coastingRangeTime += 1;	
+			    } else {
+			      this.coastingRangeTime = 0;
+			    }
+		  	    // If we have been coasting for lets say 10 seconds = 70 beats.
+			    if (this.coastingRangeTime > 69) {
+			      this.findParkingMaybe();
+			    }
+			  }
+
+			else {
 			// Now remove subtract the cut off value and cutoff + 20 value.
 			var nowRemoveVal = this.getSpeed(this.startNow);
-			this.sumNowSpeed += this.getSpeed(this.startNow + 20) - nowRemoveVal;			
+			this.sumNowSpeed += this.getSpeed(this.startNow + speedRange) - nowRemoveVal;			
 
 			var slowRemoveVal = this.getSpeed(this.startSlow);
-			this.sumSlowSpeed += this.getSpeed(this.startSlow + 20) - slowRemoveVal;
+			this.sumSlowSpeed += this.getSpeed(this.startSlow + speedRange) - slowRemoveVal;
 
 			// Test differences in average speeds.
-			var avgNow = this.sumNowSpeed / 20;
-			var avgSlow = this.sumSlowSpeed / 20;
+			var avgNow = this.sumNowSpeed / speedRange;
+			var avgSlow = this.sumSlowSpeed / speedRange;
 			var objFunc = avgNow - avgSlow;
 			var averageSpeedIsReasonable = avgSlow < 18;
 			if (objFunc > 5 && averageSpeedIsReasonable) {
@@ -83,6 +107,7 @@
 				}
 				
 			}
+			}
 
 			}
 
@@ -92,13 +117,30 @@
 			this.startSlow += 1;
 		};
 
+		Ctrl.prototype.askGas = function() {
+		  if (this.promptedGas) { return;}
+		  // Make sure the user has passed 10 mph, then start the timer.
+		  var curSpeed = this.getSpeed(this.ind - 1);
+		  if (curSpeed > 10 && !this.startAskGas) {
+		    this.startAskGas = true;
+		    this.startAskInd = this.ind;
+		  }
+		  if (this.startAskGas) {
+		    var bigDiff = this.ind - this.startAskInd;
+		     if (bigDiff > timeTilGas) {
+			promptGas();
+			this.promptedGas = true;
+		     }
+	          }
+		};
+
 		Ctrl.prototype.findParkingMaybe = function() {
 		      var bigDiff = this.ind - this.findParkingCalled;
 		      if (bigDiff > 100) { // Wait another 10 seconds before calling another.
 		        this.findParkingCalled = 0; // can call it again.
 		      }
 		      if (this.findParkingCalled == 0) {
-		        this.findParking();
+		        promptParking();
 		        this.findParkingCalled = this.ind;
 		      }
 		};
@@ -116,9 +158,36 @@
 		        return {lat: msg.gps_status.latitude, lon: msg.gps_status.longitude};	
 		};
 
-		Ctrl.prototype.findParking = function() {
-			console.log('Finding parking at ' + this.ind);
+	 	Ctrl.prototype.curSpeed = function() {
+			var length = this.messages.length;
+			return this.getSpeed(length - 1);
 		};
+
+	 	Ctrl.prototype.curGas = function() {
+			var length = this.messages.length;
+			return this.getGas(length - 1);
+		};
+
+		Ctrl.prototype.getGas = function(i) {
+			var msg = this.messages[i];
+		  	return msg.vehicle_status.fuelLevel;
+		};
+
+		Ctrl.prototype.getLastMessage = function() {
+		  return this.messages[this.ind - 1];
+		};
+
+		if (('promptParking' in window) == false) {
+		  promptParking = function() {
+		  	console.log('Finding parking at ' + ctrl.ind);
+		  };
+		}
+
+		if (('promptGas' in window) == false) {
+		  promptGas = function() {
+		  	console.log('Prompting gas at ' + ctrl.ind);
+		  };
+		}
 
                 Ctrl.prototype.mess = function(msg) {
                   var myMsg = {index: ind, txt: msg};
@@ -179,15 +248,19 @@
 		  var speeds = [17, 10];
 		  var upTo = 0;
 		  var curInd = 0;
-		  for (var i = 0; i < 300; i++) {
+		  for (var i = 0; i < 3000; i++) {
 		    var speed = speeds[curInd] / 0.62;
 	            upTo += 1;
-		    if (upTo > 50) {speed = speeds[curInd];  upTo = 0; if (curInd == 0) {curInd = 1;} else {curInd = 0;};}
-		    var response = {vehicle_status: {speed: speed},
+		    var fuelLevel = 85 + 0.1 * i;
+		    if (upTo > 150) {speed = speeds[curInd];  upTo = 0; if (curInd == 0) {curInd = 1;} else {curInd = 0;};}
+		    var response = {vehicle_status: {speed: speed, fuelLevel: fuelLevel},
 				    gps_status: {latitude: 100, longitude: 101}};
 		    this.onMessageHandler({data: JSON.stringify(response)});
 		    //console.log(ctrl.curLatLong());
+		    //console.log(ctrl.curSpeed());
+		    //console.log(ctrl.curGas());
+		    //console.log(ctrl.getLastMessage());
 		  }
 		};
 
-		var ctrl = new Ctrl();
+		//var ctrl = new Ctrl();
